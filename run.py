ed802bf3ad7ba64cfa5a3c83dec5bebc25ccf465
MagQ98 <maggieq.quinlan@gmail.com>
2021-07-17 20:29:20 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,23 @@
-# python code goes here
+# Users potential responses
+answer_a = ["A", "a"]
+answer_b = ["B", "b"]
+answer_c = ["C", "c"]
+yes = ["Yes", "YES", "yes", "y"]
+no = ["No", "NO", "no", "n"]
+
+
+# Start the game
+def start():
+    print("Would you like to start the game?")
+    answer = input()
+    if answer in yes:
+        print("Success!")
+    elif answer in no:
+        print("Failure!")
+    else:
+        print("Please answer Yes or No")
+        start()
+
+
+# Call function to start game
+start()
